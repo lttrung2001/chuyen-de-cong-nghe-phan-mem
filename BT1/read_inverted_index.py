d110a26aee880d526f0aa3068193f240ede4d933
unknown <lt.trung2001@gmail.com>
2023-02-24 10:01:37 +0700

    up

--- BT1/read_inverted_index.py
+++ BT1/read_inverted_index.py
@@ -1,17 +1,16 @@
+from entities.InvertedIndex import InvertedIndex
 # This file use to read reverse index data
 file = open('reverse-index-data.txt', 'r')
 my_dict = {}
 for line in file:
   arr = line.split('::')
-  my_dict[arr[0]] = eval(arr[1])
+  my_dict[arr[0]] = InvertedIndex(arr[1], eval(arr[2]))
 
 # Test: visible and wendelstein and with
-s1 = my_dict["visible"]
-s2 = my_dict["wendelstein"]
-s3 = my_dict["with"]
+s1 = my_dict["visible"].s
+s2 = my_dict["wendelstein"].s
+s3 = my_dict["with"].s
 print(len(s1), len(s2), len(s3))
-print(s1)
-print(sorted(s1))
 r1 = s1 & s2
 print(r1)
 r2 = r1 & s3
--- BT1/store_inverted_index.py
+++ BT1/store_inverted_index.py
@@ -3,7 +3,7 @@
 from utils.read_vocab_file import read_vocabs
 from utils.read_doc_file import read_docs
 from utils.generate_locator_cards import generate_cards
-from entities.ReverseIndex import ReverseIndex
+from entities.InvertedIndex import InvertedIndex
 
 # Read vocabs from file
 vocab_list = read_vocabs()
@@ -19,11 +19,16 @@ cards.sort()
 # Tổng hợp danh sách thẻ định vị
 my_dict = dict()
 for card in cards:
+  # Chưa có trong dict -> khởi tạo
   if card[0] not in my_dict:
-    my_dict[card[0]] = set()
-  my_dict[card[0]].add(card[1])
-# Save to file
+    my_dict[card[0]] = InvertedIndex(0, set())
+  # Doc chưa có trong postings set -> frequency + 1
+  if card[1] not in my_dict[card[0]].s:
+    my_dict[card[0]].freq += 1
+  # Thêm doc_id vào postings set
+  my_dict[card[0]].s.add(card[1])
 file = open('reverse-index-data.txt', 'w')
+# Save to file
 for key, value in my_dict.items():
-  file.write('{0}::{1}\n'.format(key, value))
+  file.write('{0}::{1}::{2}\n'.format(key, value.freq, value.s))
 file.close()
