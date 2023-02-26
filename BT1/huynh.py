ccba81ed90fae38eb9a8f9cf8f5edd1056b991d0
boyngheo21 <huynhnqt01@gmail.com>
2023-02-26 21:36:28 +0700

    Update huynh.py

--- BT1/huynh.py
+++ BT1/huynh.py
@@ -1,4 +1,5 @@
 import numpy as np
+import re
 
 documents = [
     "The quick brown fox jumps over the lazy dog.",
@@ -6,10 +7,24 @@ documents = [
     "The lazy brown dog jumps over the quick fox."
 ]
 
+pattern = r'[.]'
+
+
 # Build a vocabulary of unique words in the documents
 vocabulary = set()
+
+for num in range(len(documents)):
+    documents[num] = re.sub(pattern, '', documents[num])
+    print(documents[num])
+
 for document in documents:
-    vocabulary.update(document.lower().split())
+    mod_string = re.sub(pattern, '', document)
+    vocabulary.update(mod_string.lower().split())
+    print(mod_string)
+
+print(vocabulary)
+
+print(documents)
 
 # Build the boolean matrix
 matrix = np.zeros((len(documents), len(vocabulary)), dtype=int)
@@ -17,13 +32,13 @@ for i, document in enumerate(documents):
     words = set(document.lower().split())
     for j, word in enumerate(vocabulary):
         if word in words:
-            matrix[i, j] = 1
+            matrix[i, j] = 1            
 
-# Print the boolean matrix
+# # Print the boolean matrix
 print(matrix)
-
 query = "brown AND dog"
 query_words = query.lower().split()
 query_vector = np.array([1 if word in query_words else 0 for word in vocabulary])
 matching_documents = np.nonzero(np.all(matrix[:, query_vector == 1], axis=1))[0]
 print(matching_documents)
+           
