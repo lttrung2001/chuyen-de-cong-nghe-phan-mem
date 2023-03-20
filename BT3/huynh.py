7d4697ccc87ff05fd50f6f99b0bab20ea3d6c138
boyngheo21 <huynhnqt01@gmail.com>
2023-03-20 23:46:44 +0700

    hihi

--- BT3/huynh.py
+++ BT3/huynh.py
@@ -1,10 +1,18 @@
 # Chuỗi ký tự dài (Huỳnh)
-file = open('inverse-index-data.txt', 'r')
-chuoi_ky_tu_dai = ''
-my_list = []
-for line in file:
-    [term, freq, postings_list] = line.split('::')
-    my_list.append((freq, len(chuoi_ky_tu_dai)))
-    chuoi_ky_tu_dai += term
-print(chuoi_ky_tu_dai[:10])
-print(my_list)
+# file = open('inverse-index-data.txt', 'r')
+import pickle
+with open('sorted-inverse-index-data.pkl', 'rb') as file:
+    my_list = pickle.load(file)
+    chuoi_ky_tu_dai = ''
+    print(my_list)
+    # print("=======================\n\n\n\n\n\n")
+    miss_like = []
+    for line in my_list:
+        # [freq, postings_list, term] = line.split(',')
+        # miss_like.append((freq,postings_list, len(chuoi_ky_tu_dai)))
+        # chuoi_ky_tu_dai += term
+        thistuple = (line[0], line[1], len(chuoi_ky_tu_dai))
+        chuoi_ky_tu_dai += line[2]
+        miss_like.append(thistuple)
+    print(miss_like)    
+    
