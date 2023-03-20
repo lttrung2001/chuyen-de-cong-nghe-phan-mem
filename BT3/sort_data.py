1ae09fe255276762d9143329ac78cbbaa792ccc5
unknown <lt.trung2001@gmail.com>
2023-03-20 22:44:43 +0700

    Create sort_data.py

---
+++ BT3/sort_data.py
@@ -0,0 +1,12 @@
+file = open('inverse-index-data.txt', 'r')
+my_list = []
+for line in file:
+    [term, freq, postings_list] = line.split('::')
+    postings_list = postings_list.removesuffix("\n")
+    my_list.append((int(freq), sorted(eval(postings_list)), term))
+file.close()
+
+import pickle
+sorted_file = open('sorted-inverse-index-data.pkl', 'wb')
+pickle.dump(my_list, sorted_file, protocol=pickle.HIGHEST_PROTOCOL)
+sorted_file.close()
